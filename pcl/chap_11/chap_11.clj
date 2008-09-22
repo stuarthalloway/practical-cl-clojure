e2cafb076b2f67104755217ac9a0f00c09c2e56f
Stuart Halloway <stu@thinkrelevance.com>
2008-09-21 23:01:12 -0400

    more ch 11 examples

--- pcl/chap_11/chap_11.clj
+++ pcl/chap_11/chap_11.clj
@@ -4,22 +4,34 @@
 (defmacro print-examples [& forms]
    `(do
       ~@(map (fn [f]
-	       `(println (format "input=> %s\n%s"
+	       `(println (format "user=> %s\n%s"
 				 (pr-str '~f)
 				 (pr-str ~f)))) forms)))
 
 (print-examples (count '(1 2 3))
 		(count [1 2 3])
 		(count #{1 2 3})
+		(count "characters")
 		(count #{:name "John"}))
 
-(print-examples (:A #{:A :B :C})
-		(:D #{:A :B :C})
-		(#{:A :B :C} :A)
-		(#{:A :B :C} :D))
+(print-examples (find {:lname "Doe", :fname "John"} :fname)
+		({:lname "Doe", :fname "John"} :fname)
+		(some #(= % 2) [1 2 3]))
 
-(print-examples (:lname {:lname "Doe", :fname "John"})
-		(:mname {:lname "Doe", :fname "John"}))
+(use 'clojure.contrib.str-utils)
+(def days (re-split #" " "Sun Mon Tues Wed Thurs Fri Sat"))
+
+(print-examples (filter #(.startsWith % "S") days)
+		(count (filter #(.startsWith % "S") days))
+		(filter (complement #(.startsWith % "S")) days)
+		(map #(if (.startsWith % "S") "Weekend!" %) days)
+		(sort days))
+
+(use 'clojure.contrib.seq-utils)
+(print-examples (take 5 (concat '(1/4 1/2) powers-of-2)))
+
+
+
 
 
 
